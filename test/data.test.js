211b806314096f1cde199ce15d9e7ac79ac111cc
sdcaulley <stephanie.caulley@gmail.com>
2017-01-30 05:48:21 -0800

    save function working with tests

--- test/data.test.js
+++ test/data.test.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 const fs = require('fs');
 const findRemove = require('find-remove');
 const dataObj = require('../lib/data.js');
+const create = require('../lib/create.js');
+const saveData = require('../lib/save.js');
 const testData = require('./test-data');
 
 
@@ -21,4 +23,14 @@ describe('testing for data manipulation and configuration', function() {
         assert.equal(output.grade, 'B');
     });
 
+    it('save checks for _id and throws and error', function() {
+        const output = saveData.save('spanish', { name: 'steve', grade: 'B', _id: 'JKL456' }, dataObj.createDataObj);
+        assert.equal(output, 'This record has an id.');
+    });
+
+    it('save creates new document when there is no _id', function() {
+        const output = saveData.save('spanish', { database: 'students', collection: 'spanish', name: 'steve', grade: 'B' }, create.createDBDir);
+        assert.equal(output.name, 'steve');
+        assert.equal(output.database, 'students');
+    });
 });
